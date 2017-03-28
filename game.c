bcd868505b1430e23b5c79d68d8383aae8635d93
aKwasny <andrzej.kwasiborski@gmail.com>
2017-03-28 13:18:57 +0200

    Kosmetyka

--- game.c
+++ game.c
@@ -17,9 +17,10 @@
 
 
 const char *usage = "Usage: \n"
-        "./a.out -s size -n number_of_iterations\n"
+        "./a.out -s size -n number_of_iterations -f\n"
         "\tsize - here you can define the size of the board. Write 30 to generate a 30x30 board. Default value is 20.\n"
-        "\tn - the number of iterations. By default it is set to 10. \n";
+        "\tn - the number of iterations. By default it is set to 10. \n"
+        "\tf - if you type it, all generations will occur right away.\n";
 
 
 int neighbours (const int* current_state, int s, int x, int y) {
@@ -38,8 +39,8 @@ int neighbours (const int* current_state, int s, int x, int y) {
 }
 
 
-int generate (int *current_state, int *next_state, int s) {
-	int i, j, cell, neighbour_no;
+void generate (int *current_state, int *next_state, int s) {
+	int i, j, neighbour_no;
 	for_xy {
 		neighbour_no = neighbours(current_state, s, i, j);
 
@@ -79,19 +80,20 @@ void get_terminal_size(int * ncols, int * nrows) {
 
 
 int main(int argc, char **argv) {
-	int i, s, n, *current_state, *next_state, *tmp, opt, ncols, nrows; /*s - size, n - no. of generations*/
+	int i, s, n, f, *current_state, *next_state, *tmp, opt, ncols, nrows; /*s - size, n - no. of generations, f - fast generation*/
 	
 	n = 10;
 	s = 20;
+    f = 0;
 
     get_terminal_size(&ncols, &nrows);
 
-    if (argc < 3) {
+    if (argc < 1) {
         puts(usage);
         return EXIT_FAILURE;
     }
 	
-	while ((opt = getopt(argc, argv, "s:n:")) != -1) {
+	while ((opt = getopt(argc, argv, "s:n:f")) != -1) {
         switch (opt) {
             case 's':
                 s = atoi(optarg);
@@ -103,6 +105,9 @@ int main(int argc, char **argv) {
             case 'n': 
                 n = atoi(optarg);
                 break;
+            case 'f':
+                f = 1;
+                break;
             default:
                 fprintf(stderr, "%s: Incorrect try of induction. %c was incorrect. Please read the following and try again.\n", argv[0], opt);
                 puts(usage);
@@ -113,14 +118,14 @@ int main(int argc, char **argv) {
 	current_state = malloc (s*s*sizeof(*current_state));
 	next_state = malloc (s*s*sizeof(*next_state));
 	
-	srand(time(NULL));
+	srand((unsigned int) time(NULL));
 	
 	for(i = 0; i < s*s; i++){
 		current_state[i] = rand() % 2;
 	}
 
     fprintf(stdout, "\nGeneration started. Matrix will be %dx%d. There will be %d iterations.\n\n", s, s, n);
-    sleep (1);
+    if (f == 0) sleep (1);
 	
 	for(i = 0; i < n; i++){
 		print_board(current_state, s);
@@ -129,9 +134,15 @@ int main(int argc, char **argv) {
 		current_state = next_state;
 		next_state = tmp;
 		fprintf(stdout, "\n\n");
-        if (i != n-1) sleep(2);
-        if (i == n-1) fprintf(stdout, "%d generations complete. The programme will end now. Bye!\n", n);
-        sleep(1);
+        if ((i != n-1) && (f == 0)) sleep(2);
+        if (i == n-1) {
+            fprintf(stdout, "%d generations complete. The programme will end now. Bye!\n", n);
+            if (f == 0) sleep(1);
+        }
 	}
+
+    free(current_state);
+    free(next_state);
+
 	return 0;
 }
